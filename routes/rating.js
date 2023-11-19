58c07f3c5713f53c4414a5161df395625124c690
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2023-11-19 21:07:52 +0300

    Exportdata (#7)
    * exporting rating data changes at /routes/rating.js
    
    * created new export.js files for controllers,routes
    
    * Exporting data to JSON is handled. Maybe exporting to CSV can be handled.
    
    ---------
    
    Co-authored-by: acagatay <acagatay@sabanciuniv.edu>

--- routes/rating.js
+++ routes/rating.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { rateSong, rateAlbum , rateArtist} = require('../controllers/rating');
+const mongoose = require('mongoose');
+const Rating = require('../models/Rating'); 
+const { rateSong, rateAlbum, rateArtist } = require('../controllers/rating');
 
 // Rate a song
 router.post('/rateSong', rateSong);
