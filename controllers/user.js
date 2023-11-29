414d755124dacce876073689c1b824635f976d2e
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2023-11-29 21:03:56 +0300

    Emin user allow recommendations to friends (#10)
    * feature added: Spotify Recommendations
    
    * Feature Added: User can not allow or disallow in terms of letting their musics to be recommended to their friends.

--- controllers/user.js
+++ controllers/user.js
@@ -162,3 +162,59 @@ exports.deleteSong = async (req, res, next) => {
     });
   }
 };
+
+exports.allowRecommendationsToFriends = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "No user is found",
+        success: false,
+      });
+    }
+
+    user.allowFriendRecommendations = true;
+
+    await user.save();
+
+    return res.status(200).json({
+      message:
+        "User successfully updated. Now, your friends CAN see your musics!",
+      success: true,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+      success: false,
+    });
+  }
+};
+
+exports.disallowRecommendationsToFriends = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "No user is found",
+        success: false,
+      });
+    }
+
+    user.allowFriendRecommendations = false;
+
+    await user.save();
+
+    return res.status(200).json({
+      message:
+        "User successfully updated. Now, your friends CANNOT see your musics!",
+      success: true,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+      success: false,
+    });
+  }
+};
