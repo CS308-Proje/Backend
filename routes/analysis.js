045b2bf4977bd75ce6eb910623594538a1c75453
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2023-12-06 13:36:24 +0300

    Canvas problem is handled.

--- routes/analysis.js
+++ routes/analysis.js
@@ -5,14 +5,10 @@ const router = express.Router();
 
 const isAuth = require("../middlewares/isAuth");
 
-router.get("/chart", isAuth.protect, analysisController.createAnalysis);
-
 router.get(
   "/song-analysis",
   isAuth.protect,
   analysisController.createAnalysisBasedOnSongs
 );
-
-router.get("/song-canvas", isAuth.protect, analysisController.songCanvas);
 
 module.exports = router;
