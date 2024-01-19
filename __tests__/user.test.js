2fa24faad51a942755a2292a1a29058dcfc999ee
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2024-01-19 20:18:13 +0300

    Added error handling for analysis part. More test are added.

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,21 +1,26 @@
-require('dotenv').config({ path: './config/config.env' });
-const request = require('supertest');
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('../models/User');
-const userController = require('../controllers/user'); 
-const {protect, authorize} = require('../middlewares/isAuth');
-const { login } = require('../controllers/authentication');
+require("dotenv").config({ path: "./config/config.env" });
+const request = require("supertest");
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const userController = require("../controllers/user");
+const { protect, authorize } = require("../middlewares/isAuth");
+const { login } = require("../controllers/authentication");
 
 const app = express();
 app.use(express.json());
 
 // Define your user routes
-app.post('/login', login);
-app.post('/users', protect, authorize("admin") ,userController.addUser);
-app.delete('/users/:id', protect , authorize("admin") ,userController.deleteUser);
+app.post("/login", login);
+app.post("/users", protect, authorize("admin"), userController.addUser);
+app.delete(
+  "/users/:id",
+  protect,
+  authorize("admin"),
+  userController.deleteUser
+);
 
-describe('User API', () => {
+describe("User API", () => {
   let authToken;
   let userIdtoDelete;
   beforeAll(async () => {
@@ -24,15 +29,11 @@ describe('User API', () => {
       //useUnifiedTopology: true,
     });
 
+    const loginRes = await request(app).post("/login").send({
+      email: "testuser1702056245215@example.com", //existing user form database
+      password: "password123",
+    });
 
-    const loginRes = await request(app)
-      .post('/login')
-      .send({
-        email: 'testuser1702056245215@example.com', //existing user form database
-        password: 'password123', 
-      });
-
-    
     expect(loginRes.statusCode).toEqual(200);
     authToken = loginRes.body.token;
   });
@@ -41,39 +42,30 @@ describe('User API', () => {
     await mongoose.connection.close();
   });
 
-  it('should add a new user', async () => {
+  it("should add a new user", async () => {
     const newUser = {
-      name: 'Test User',
-      username: 'testuser',
+      name: "Test User",
+      username: "testuser",
       email: `testuser${Date.now()}@example.com`,
-      password: 'password123',
+      password: "password123",
     };
 
-    userIdtoDelete = newUser._id;
-
     const res = await request(app)
-      .post('/users')
-      .set('Authorization', `Bearer ${authToken}`)
+      .post("/users")
+      .set("Authorization", `Bearer ${authToken}`)
       .send(newUser);
 
+    userIdtoDelete = res.body.user._id;
 
-      
-
-      userIdtoDelete = res.body.user._id;
-
-    expect(res.statusCode).toEqual(201); 
-    expect(res.body).toHaveProperty('user');
-
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("user");
   });
 
-  it('should delete a user', async () => {
-
+  it("should delete a user", async () => {
     const res = await request(app)
       .delete(`/users/${userIdtoDelete}`)
-      .set('Authorization', `Bearer ${authToken}`)
-      
+      .set("Authorization", `Bearer ${authToken}`);
 
-    expect(res.statusCode).toEqual(200); 
-
+    expect(res.statusCode).toEqual(200);
   });
 });
