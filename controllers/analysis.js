5c6cd4c9ddbc487806547e5c01af2b2549e3344f
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2024-01-20 16:24:18 +0300

    bugfix: Data export

--- controllers/analysis.js
+++ controllers/analysis.js
@@ -30,9 +30,7 @@ exports.createAnalysisBasedOnSongs = async (req, res, next) => {
       });
     }
 
-
     if (type === null || type === undefined || type === "") {
-
       return res.status(400).json({
         error: "Please enter a type. Type can be song, album or artist.",
         success: false,
@@ -497,9 +495,7 @@ exports.averageRatingForMonth = async (req, res, next) => {
     const ratings = await Rating.find({
       userId: userId,
       createdAt: {
-
         $gte: lastMonthStart,
-
       },
     });
 
