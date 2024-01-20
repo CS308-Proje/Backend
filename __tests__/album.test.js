32737a0cdc59a8ebe053e3237bb89c01758d73cb
acagatay <acagatay@sabanciuniv.edu>
2024-01-20 23:51:01 +0300

    album + artist

--- __tests__/album.test.js
+++ __tests__/album.test.js
@@ -5,8 +5,11 @@ const mongoose = require("mongoose");
 const albumController = require("../controllers/album");
 const authenticationController = require("../controllers/authentication");
 const { protect } = require("../middlewares/isAuth");
+const { addSongThatIsNotFromSpotifyAPI } = require("../controllers/song");
 const Album = require("../models/Album");
 const User = require("../models/User");
+const Song = require("../models/Song");
+const Artist = require("../models/Artist");
 const app = express();
 
 app.use(express.json());
@@ -15,100 +18,101 @@ app.get("/albums", protect, albumController.getAlbums);
 app.get("/albums/:id", protect, albumController.getAlbum);
 app.delete("/albums/:id", protect, albumController.deleteAlbum);
 app.put("/albums/:id", protect, albumController.updateAlbum);
+app.post("/add-song-not-from-spotify", protect, addSongThatIsNotFromSpotifyAPI);
 
 describe("Album API", () => {
-    let authToken;
-    let testAlbumId;
+  let authToken;
+  let testAlbumId;
+  let testSongId;
+  let testArtistId;
 
-
-    beforeAll(async () => {
+  beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     // Log in to get a token
     const loginRes = await request(app).post("/login").send({
-        email: "testuser1702056245215@example.com",
-        password: "password123",
+      email: "testuser1702056245215@example.com",
+      password: "password123",
     });
 
     expect(loginRes.statusCode).toEqual(200);
     authToken = loginRes.body.token;
-    });
+  });
 
-    afterAll(async () => {
+  afterAll(async () => {
+    await Song.deleteMany({ _id: testSongId });
+    await Artist.deleteMany({ _id: testArtistId });
     await mongoose.connection.close();
-    });
+  });
 
-    it("should get all albums", async () => {
-        const res = await request(app)
-            .get("/albums")
-            .set("Authorization", `Bearer ${authToken}`);
+  it("should get all albums", async () => {
+    const res = await request(app)
+      .get("/albums")
+      .set("Authorization", `Bearer ${authToken}`);
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty("albums");
-        expect(res.body.success).toEqual(true);
-    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("albums");
+    expect(res.body.success).toEqual(true);
+  });
 
-    it("should add a song", async () => {
-        const songData = {
-          songName: "Test Song1",
-          mainArtistName: "Test Artist1",
-          albumName: "Test Album1",
-          featuringArtistNames: [],
-          release_date: "2021-01-02",
-        };
-    
-        const res = await request(app)
-          .post("/add-song-not-from-spotify")
-          .set("Authorization", `Bearer ${authToken}`)
-          .send(songData);
-
-        console.log(res.body)
-        testAlbumId = res.body.song[0].albumId;
-        
-    
-        expect(res.statusCode).toEqual(201);
-        expect(res.body).toHaveProperty("song");
-      });
-
-    it("should get a single album", async () => {
-        const res = await request(app)
-            .get(`/albums/${testAlbumId}`)
-            .set("Authorization", `Bearer ${authToken}`);
-
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.success).toEqual(true);
-        expect(res.body).toHaveProperty("album");
-        expect(res.body.album[0]._id).toEqual(testAlbumId);
-
-    });
-
-    it("should update an album", async () => {
-        const updateAlbumData = {
-        name: "Updated Test Album",
-
+  it("should add a song", async () => {
+    const songData = {
+      songName: "Test Song1",
+      mainArtistName: "Test Artist1",
+      albumName: "Test Album1",
+      featuringArtistNames: [],
+      release_date: "2021-01-02",
     };
 
-        const res = await request(app)
-            .put(`/albums/${testAlbumId}`)
-            .set("Authorization", `Bearer ${authToken}`)
-            .send(updateAlbumData);
+    const res = await request(app)
+      .post("/add-song-not-from-spotify")
+      .set("Authorization", `Bearer ${authToken}`)
+      .send(songData);
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.album.name).toEqual("Updated Test Album");
+    console.log(res.body);
+    testAlbumId = res.body.song.albumId;
+    testSongId = res.body.song._id;
+    testArtistId = res.body.song.mainArtistId;
 
-    });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("song");
+  });
 
-    it("should delete an album", async () => {
-        const res = await request(app)
-            .delete(`/albums/${testAlbumId}`)
-            .set("Authorization", `Bearer ${authToken}`);
+  it("should get a single album", async () => {
+    const res = await request(app)
+      .get(`/albums/${testAlbumId}`)
+      .set("Authorization", `Bearer ${authToken}`);
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.success).toEqual(true);
-        expect(res.body.message).toEqual("Album is deleted.");
-    });
-  
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toEqual(true);
+    expect(res.body).toHaveProperty("album");
+    expect(res.body.album[0]._id).toEqual(testAlbumId);
+  });
+
+  it("should update an album", async () => {
+    const updateAlbumData = {
+      name: "Updated Test Album",
+    };
+
+    const res = await request(app)
+      .put(`/albums/${testAlbumId}`)
+      .set("Authorization", `Bearer ${authToken}`)
+      .send(updateAlbumData);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.album.name).toEqual("Updated Test Album");
+  });
+
+  it("should delete an album", async () => {
+    const res = await request(app)
+      .delete(`/albums/${testAlbumId}`)
+      .set("Authorization", `Bearer ${authToken}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toEqual(true);
+    expect(res.body.message).toEqual("Album is deleted.");
+  });
 });
---
+++ __tests__/artist.test.js
@@ -0,0 +1,120 @@
+require("dotenv").config({ path: "./config/config.env" });
+const express = require("express");
+const request = require("supertest");
+const mongoose = require("mongoose");
+const artistContoller = require("../controllers/artist");
+const authenticationController = require("../controllers/authentication");
+const { protect } = require("../middlewares/isAuth");
+const { addSongThatIsNotFromSpotifyAPI } = require("../controllers/song");
+const Album = require("../models/Album");
+const User = require("../models/User");
+const Song = require("../models/Song");
+const Artist = require("../models/Artist");
+const app = express();
+
+app.use(express.json());
+app.post("/login", authenticationController.login);
+app.get("/artists", protect, artistContoller.getArtists);
+app.get("/artists/:id", protect, artistContoller.getArtist);
+app.delete("/artists/:id", protect, artistContoller.deleteArtist);
+app.put("/artists/:id", protect, artistContoller.updateArtist);
+app.post("/add-song-not-from-spotify", protect, addSongThatIsNotFromSpotifyAPI);
+
+
+describe("Artist API", () => {
+  let authToken;
+  let testAlbumId;
+  let testSongId;
+  let testArtistId;
+
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Log in to get a token
+    const loginRes = await request(app).post("/login").send({
+      email: "testuser1702056245215@example.com",
+      password: "password123",
+    });
+
+    expect(loginRes.statusCode).toEqual(200);
+    authToken = loginRes.body.token;
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it("should get all artists", async () => {
+    const res = await request(app)
+      .get("/artists")
+      .set("Authorization", `Bearer ${authToken}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("artists");
+    expect(res.body.success).toEqual(true);
+  });
+
+  it("should add a song", async () => {
+    const songData = {
+      songName: "Test Song1",
+      mainArtistName: "Test Artist1",
+      albumName: "Test Album1",
+      featuringArtistNames: [],
+      release_date: "2021-01-02",
+    };
+
+    const res = await request(app)
+      .post("/add-song-not-from-spotify")
+      .set("Authorization", `Bearer ${authToken}`)
+      .send(songData);
+
+    testAlbumId = res.body.song.albumId;
+    testSongId = res.body.song._id;
+    testArtistId = res.body.song.mainArtistId;
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("song");
+  });
+
+  it("should get a single artist", async () => {
+    const res = await request(app)
+      .get(`/artists/${testArtistId}`)
+      .set("Authorization", `Bearer ${authToken}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toEqual(true);
+    expect(res.body).toHaveProperty("artist");
+    expect(res.body.artist[0]._id).toEqual(testArtistId);
+  });
+
+  it("should update an artist", async () => {
+    const updateArtistData = {
+      artistName: "Updated Test Artist",
+    };
+    
+    const res = await request(app)
+      .put(`/artists/${testArtistId}`)
+      .set("Authorization", `Bearer ${authToken}`)
+      .send(updateArtistData);
+    console.log(res.body)
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("artist");
+    expect(res.body.artist.artistName).toEqual("Updated Test Artist");
+  });
+
+  it("should delete an artist", async () => {
+    const res = await request(app)
+      .delete(`/artists/${testArtistId}`)
+      .set("Authorization", `Bearer ${authToken}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toEqual(true);
+    expect(res.body.message).toEqual("Artist is deleted.");
+
+    await Song.deleteMany({ _id: testSongId });
+    await Album.deleteMany({ _id: testAlbumId });
+  });
+});
