e439b103cc5368cb7bbf4b55dab56ab2ca7ba6d4
Emin Üstün <99042047+eminbustun@users.noreply.github.com>
2024-01-21 22:32:51 +0300

    Spotify Error fixed.

--- config/spotifyAPI.js
+++ config/spotifyAPI.js
@@ -2,8 +2,8 @@ const request = require("request-promise");
 // BQAxPH3A8ztKaJH7pL2lUWdVDMHkaLg1_-y37RFNNBRv-VNeEWVJFUITbLyWkZkUy-QnyUM5A6O4XbGb4CEdLvPJysLOm9H7SVRedzDkZ7F_Su0j9Gs
 
 const dotenv = require("dotenv").config({ path: "./config.env" });
-const client_id = "8ed5bfd11e874bfe8077c3ea6ee4b3e4";
-const client_secret = "07572e28d7e34de9967e3aeabf39dd9b";
+const client_id = "d70d599bc82a44f6a165a9b4516f85de";
+const client_secret = "ee0cda9e6c4249d1aa400d2a147567a8";
 
 var authOptions = {
   url: "https://accounts.spotify.com/api/token",
